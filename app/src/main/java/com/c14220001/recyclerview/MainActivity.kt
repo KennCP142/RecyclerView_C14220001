a9dadc7cee07dac1742fa05b92f8413ac52ded94
HedgeHawk808 <kenncahyaputra142@gmail.com>
2024-11-09 18:43:09 +0700

    updated code

--- app/src/main/java/com/c14220001/recyclerview/MainActivity.kt
+++ app/src/main/java/com/c14220001/recyclerview/MainActivity.kt
@@ -1,19 +1,80 @@
 package com.c14220001.recyclerview
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class adapterRecView (private val listWayang : ArrayList<wayang>) : RecyclerView
-.Adapter<adapterRecView.ListViewHolder> () {
+private lateinit var _rvWayang : RecyclerView
 
+class adapterRecView (private val listWayang : ArrayList<wayang>) : RecyclerView
+    .Adapter<adapterRecView.ListViewHolder> () {
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var _namaWayang = itemView.findViewById<TextView>(R.id.namaWayang)
+        var _karakterWayang = itemView.findViewById<TextView>(R.id.karakterWayang)
+        var _deskripsiWayang = itemView.findViewById<TextView>(R.id.deskripsiWayang)
+        //var _gambarWayang = itemView.findViewById<TextView>(R.id.gambarWayang)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler,parent,false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listWayang.size
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        var wayang = listWayang[position]
+        holder._namaWayang.setText(wayang.nama)
+        holder._deskripsiWayang.setText(wayang.deskripsi)
+        holder._karakterWayang.setText(wayang.karakter)
+    }
 }
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _nama : Array<String>
+    private lateinit var _karakter : Array<String>
+    private lateinit var _deskripsi : Array<String>
+    private lateinit var _gambar : Array<String>
+
+    private var arWayang = arrayListOf<wayang>()
+
+    fun SiapkanData() {
+        _nama = resources.getStringArray(R.array.namaWayang)
+        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
+        _gambar = resources.getStringArray(R.array.gambarWayang)
+    }
+
+    fun TambahData() {
+        for (position in _nama.indices) {
+            val data = wayang(
+                _gambar[position],
+                _nama[position],
+                _karakter[position],
+                _deskripsi[position]
+            )
+            arWayang.add(data)
+        }
+    }
+
+    fun TampilkanData() {
+        _rvWayang.layoutManager = LinearLayoutManager(this)
+        _rvWayang.adapter = adapterRecView(arWayang)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,5 +84,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        _rvWayang = findViewById<RecyclerView>(R.id.rvWayang)
+        SiapkanData()
+        TambahData()
+        TampilkanData()
     }
 }

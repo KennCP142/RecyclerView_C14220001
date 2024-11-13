75db8409dca9073c7244b4a94862004fed699889
HedgeHawk808 <kenncahyaputra142@gmail.com>
2024-11-13 19:38:47 +0700

    10.0.2 - 10.0.4 DONE

--- app/src/main/java/com/c14220001/recyclerview/MainActivity.kt
+++ app/src/main/java/com/c14220001/recyclerview/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.c14220001.recyclerview
 
 import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.hardware.Camera.PictureCallback
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.core.view.ViewCompat
@@ -24,11 +32,23 @@ private lateinit var _rvWayang : RecyclerView
 
 class adapterRecView (private val listWayang : ArrayList<wayang>) : RecyclerView
     .Adapter<adapterRecView.ListViewHolder> () {
+        private lateinit var onItemClickCallback: OnItemClickCallback
+
+    interface OnItemClickCallback {
+        fun onItemCLicked(data:wayang)
+        fun delData(position: Int)
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var _namaWayang = itemView.findViewById<TextView>(R.id.namaWayang)
         var _karakterWayang = itemView.findViewById<TextView>(R.id.karakterWayang)
         var _deskripsiWayang = itemView.findViewById<TextView>(R.id.deskripsiWayang)
         var _gambarWayang = itemView.findViewById<ImageView>(R.id.gambarWayang)
+        var _btnHapus = itemView.findViewById<Button>(R.id.btnHapus)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -49,26 +69,35 @@ class adapterRecView (private val listWayang : ArrayList<wayang>) : RecyclerView
         Picasso.get()
             .load(wayang.foto)
             .into(holder._gambarWayang)
+
+        holder._gambarWayang.setOnClickListener{
+            onItemClickCallback.onItemCLicked(listWayang[position])
+        }
+
+        holder._btnHapus.setOnClickListener{
+            onItemClickCallback.delData(position)
+        }
     }
 }
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var _nama : Array<String>
-    private lateinit var _karakter : Array<String>
-    private lateinit var _deskripsi : Array<String>
-    private lateinit var _gambar : Array<String>
+    private lateinit var _nama : MutableList<String>
+    private lateinit var _karakter : MutableList<String>
+    private lateinit var _deskripsi : MutableList<String>
+    private lateinit var _gambar : MutableList<String>
 
     private var arWayang = arrayListOf<wayang>()
 
     fun SiapkanData() {
-        _nama = resources.getStringArray(R.array.namaWayang)
-        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
-        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
-        _gambar = resources.getStringArray(R.array.gambarWayang)
+        _nama = resources.getStringArray(R.array.namaWayang).toMutableList()
+        _deskripsi = resources.getStringArray(R.array.deskripsiWayang).toMutableList()
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang).toMutableList()
+        _gambar = resources.getStringArray(R.array.gambarWayang).toMutableList()
     }
 
     fun TambahData() {
+        arWayang.clear()
         for (position in _nama.indices) {
             val data = wayang(
                 _gambar[position],
@@ -81,8 +110,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun TampilkanData() {
-        _rvWayang.layoutManager = StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL)
+        _rvWayang.layoutManager = LinearLayoutManager(this)
         _rvWayang.adapter = adapterRecView(arWayang)
+
+        var adapterWayang = adapterRecView(arWayang)
+        _rvWayang.adapter = adapterWayang
+
+        adapterWayang.setOnItemClickCallback(object : adapterRecView.OnItemClickCallback{
+            override fun onItemCLicked(data: wayang) {
+                Toast.makeText(this@MainActivity,data.nama,Toast.LENGTH_LONG)
+                    .show()
+
+                val intent = Intent(this@MainActivity, detWayang::class.java)
+                intent.putExtra("kirimData", data)
+                startActivity(intent)
+            }
+
+            override fun delData(position: Int) {
+                AlertDialog.Builder(this@MainActivity)
+                    .setTitle("HAPUS DATA")
+                    .setMessage("Apakah benar Data " +  _nama[position] + " akan dihapus ?")
+                    .setPositiveButton(
+                        "HAPUS",
+                        DialogInterface.OnClickListener{dialog,which ->
+                            _gambar.removeAt(position)
+                            _nama.removeAt(position)
+                            _deskripsi.removeAt(position)
+                            _karakter.removeAt(position)
+                            TambahData()
+                            TampilkanData()
+                        }
+                    )
+                    .setNegativeButton(
+                        "BATAL",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Data Batal Dihapus",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    ).show()
+            }
+        })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
